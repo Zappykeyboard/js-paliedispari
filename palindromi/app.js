07d3d1654852a93c7fd0ba98a94e6781e2d5553d
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-08 16:22:02 +0200

    aggiunti file aggiunta logica palindromo

---
+++ palindromi/app.js
@@ -0,0 +1,13 @@
+//determina se una parola è palindroma
+function isPalindrome(word) {
+    var reversed = word.split("").reverse().join("");
+    if (word === reversed) {
+      return "L'input è un palindromo!";
+    } else {
+      return "L'input non è un palindromo"
+    }
+}
+
+var input = prompt("Inserisci una parola o frase");
+
+console.log(isPalindrome(""));
