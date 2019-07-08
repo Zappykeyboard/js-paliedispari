69d8bc4792397a11a22b1d987d944de117a8434b
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-08 16:37:26 +0200

    aggiunta logica

---
+++ paridispari/app.js
@@ -0,0 +1,42 @@
+//utente sceglie se numero sarà pari o dispari
+var input = prompt("scrivi 'pari' oppure 'dispari'");
+
+//funzione per generare un numero random (da 1 a 5).
+function generaRandom(){
+  return Math.floor(Math.random() * 5 + 1);
+}
+
+//generiamo i numeri
+var primoNumero = generaRandom();
+console.log("primo numero è " + primoNumero);
+
+var secondoNumero = generaRandom();
+console.log("secondo numero numero è " + secondoNumero);
+
+var sum = primoNumero + secondoNumero;
+
+//funzione per determinare se numero è vero o falso
+function isEven(n){
+  if (n % 2 === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//determino se utente ha vinto
+if (isEven(sum)){
+  console.log("la somma dei numeri è pari");
+  if (input.includes("pari")){
+    console.log("utente ha scelto pari ed ha vinto!");
+  } else {
+    console.log("utente ha scelto dispari ed ha perso!");
+  }
+} else {
+  console.log("la somma dei numeri è dispari");
+  if (input.includes("dispari")){
+    console.log("utente ha scelto dispari ed ha vinto!");
+  } else {
+    console.log("utente ha scelto pari ed ha perso!");
+  }
+}
